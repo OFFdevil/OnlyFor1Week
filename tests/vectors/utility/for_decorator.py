e91772db2f67bcc710fbcb76238df0b54a9bea83
Liza <liza.grankina@gmail.com>
2023-05-28 20:00:00 +0300

    рендеринг небесной сферы

--- tests/vectors/utility/for_decorator.py
+++ tests/vectors/utility/for_decorator.py
@@ -1,10 +1,10 @@
-def for_range(name, begin, end, step=1):  # вместо цикла написали ещё один декоратор
-    def decorator(foo):  # декоратор передаем функцию foo
+def for_range(name, enum):  # вместо цикла написали ещё один декоратор
+    def decorator(foo):  # декоратору передаем функцию foo
         def decorated(*args, **kwargs):  # оболочка декоратора, которая принимает на вход все значения функции
             if not kwargs:
                 kwargs = {}
             results = []  # создали пустой список
-            for i in range(begin, end, step):  # принимаем границы цикла из декоратора
+            for i in enum:  # принимаем границы цикла из декоратора
                 kwargs[name] = i
                 results.append(foo(*args, **kwargs))  # все аргументы функции записываем в results
             return results
@@ -14,8 +14,8 @@ def for_range(name, begin, end, step=1):  # вместо цикла написа
     return decorator
 
 
-@for_range('i', 1, 100)
+@for_range('i', range(0, 100))
 def foo(i):
-    print(i)  # выводим цифры от 1 до 100
+    print(i)  # выводим цифры от 0 до 100
 
 # foo()
