c0485750b3af48acbcdc1d7f2508c7c4c4238548
Liza <liza.grankina@gmail.com>
2023-06-01 16:07:45 +0300

    новые файлы для работы

--- tests/vectors/base_test.py
+++ tests/vectors/base_test.py
@@ -6,7 +6,7 @@ from vectors.vector import Vector
 
 
 class VectorTest(unittest.TestCase):
-    def setUp(self): #создает вектора
+    def setUp(self):  # создает вектора
         self.x = 2
         self.y = 3
         self.z = 4
@@ -15,21 +15,21 @@ class VectorTest(unittest.TestCase):
         self.a = Vector(2, 100, 4)
         self.b = Vector(2, 100, 4)
 
-    def test_creation(self): #тест функции создания векторов
+    def test_creation(self):  # тест функции создания векторов
         self.assertEqual(self.v.x, self.x)
         self.assertEqual(self.v.y, self.y)
         self.assertEqual(self.v.z, self.z)
 
-    def test_equal(self): #проверяет равны ли два вектора
+    def test_equal(self):  # проверяет равны ли два вектора
         self.assertEquals(self.a, self.b)
 
-    def test_unequal(self): #проверяет что два вектора не равны
+    def test_unequal(self):  # проверяет что два вектора не равны
         self.assertNotEquals(self.a, self.v)
 
-    def test_length(self): #проверяет функцию вычисления длины вектора
+    def test_length(self):  # проверяет функцию вычисления длины вектора
         self.assertEquals(self.a.length, sqrt(self.a.x ** 2 + self.a.y ** 2 + self.a.z ** 2))
 
-    def test_str(self): #проверяет функцию возврата координат вектора в виде строки
+    def test_str(self):  # проверяет функцию возврата координат вектора в виде строки
         self.assertEquals(str(self.a), "(2, 100, 4)")
 
 
