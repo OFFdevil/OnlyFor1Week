88c161bfe23b10a2893f1a86985fa03da5ac3ecb
nasty <emelyanova1401@mail.ru>
2023-06-07 03:13:38 +0300

    несущественные изменения(refact. crendr.)

--- geometry/avector.py
+++ geometry/avector.py
@@ -27,8 +27,8 @@ class Equatorial(AngleVector):  # в экваториальной системе
     def apply_time(self, sidereal_time):  # ?
         return Equatorial(self.alpha + sidereal_time, self.delta)
 
-    def to_horizontal_system(self, latitude, sidereal_time):  # перевод из экваториальной в горизонтальную
-        timed = self.apply_time(sidereal_time)
+    def to_horizontal_system(self, latitude, star_time_degree): #перевод из экваториальной в горизонтальную
+        timed = self.apply_time(star_time_degree)
         d = math.radians(timed.delta)
         t = math.radians(timed.alpha)
         f = math.radians(latitude)
--- stars/skybase.py
+++ stars/skybase.py
@@ -4,22 +4,22 @@ from geometry.avector import Equatorial
 from stars.star import Star
 
 
-class SkyBase:
-    def __init__(self, stars):  # инициализация
+class SkyBase:  # класс-база данных со всеми звездами
+    def __init__(self, stars):  # атрибуты классы - звезды и созвездия
         self._stars = stars
-        self._constellations = {star.constellation: [] for star in stars}  # созвездия - это массив звезд
+        self._constellations = {star.constellation: [] for star in stars}  # созвездие - это массив звезд
         for star in stars:
             self._constellations[star.constellation].append(star)
 
     @property
     def constellations(self):  # геттер.
-        return self._constellations.keys()  # функция keys по сути возвращает ключи словаря
+        return self._constellations.keys()  # функция keys по сути возвращает ключи словаря(то есть названия созвездий)
 
-    def get_stars(self, constellations):
+    def get_stars(self, avaible_constellations: set):  # передаем set из созвездий
         stars = []
-        for constellation in constellations:
+        for constellation in avaible_constellations:
             if not constellation in self._constellations:
                 continue
-            for star in self._constellations[constellation]:
+            for star in self._constellations[constellation]:  # и из всех выбираем только те
                 stars.append(star)
             return stars
